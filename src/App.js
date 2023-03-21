732ae474c4e7877cd2d232bebd9d8ab33f66cfae
shrutigarg1210 <shrutigarg749@gmail.com>
2023-03-21 11:17:04 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      </BrowserRouter>
+      
     </div>
 
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Onthegomechanic</a>
+    <a className="navbar-brand" href="/">OnTheGoMechanic</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -19,17 +19,21 @@ const Navbar = () => {
         <li className="nav-item">
           <a className="nav-link" href="/">Services</a>
         </li>
-        {/* <li className="nav-item dropdown">
+        <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Registration Form
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li> */}
+            <li><a className="dropdown-item" href="/">User</a></li>
+
+            <Link to = { "/Mechanicform "}>Mechanic</Link>
+            {/* <li>
+              <Link to = {"/Mechanicform"}>Mechanic </Link>
+              </li> */}
+             {/* <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="/">Something else here</a></li> */}
+           </ul>
+        </li>  
         {/* <li className="nav-item">
           <a className="nav-link disabled">Disabled</a>
         </li> */}
