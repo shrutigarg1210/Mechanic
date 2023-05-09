29d8a4f9a15fc8a1f94c33a8fc1cba239e00e858
shrutigarg1210 <shrutigarg749@gmail.com>
2023-05-09 20:20:18 +0530

    about

--- src/components/AboutDetails/Aboutdata.js
+++ src/components/AboutDetails/Aboutdata.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Aboutdata = () => {
   return (
     <div>
-      Aboutdata
-      </div>
-  )
-}
+      Enough of Standing in the middle of road with opened bonuts of cars and
+      waiting for your sibling to arrive with a mechanic.! Here we are.... with
+      our loyal services offering you the most amazing website with which you
+      will be able to repair your car anywhere anytime without bothering anyone
+      to come and pick you and your car up.! All you have to do is register give
+      us your location and our mechanic will be there for you and your car and
+      will fix your car and mood as well. We assure you won't bother anyone for
+      your car situation because we are here for you with our services, Bother
+      people for other things and not for your car to fix.
+    </div>
+  );
+};
 
-export default Aboutdata
+export default Aboutdata;
