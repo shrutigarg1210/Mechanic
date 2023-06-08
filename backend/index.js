665dd096372a5e608c3f0d7d1c7de4d36c6399a5
Saksham sharma <ssaksham952@gmail.com>
2023-06-08 06:33:58 -0700

    carousel set

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,72 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
-const mongoose = require('mongoose');
-const cors = require("cors")
-const bodyParser = require("body-parser");
-const PORT = 4000;
+// const app = express();
+// const mongoose = require('mongoose');
+// const cors = require("cors")
+// const bodyParser = require("body-parser");
+// const PORT = 4000;
 
 
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // app.use(bodyParser.json())
-app.use(cors());
-app.use(express.json());
-mongoose.connect("mongodb+srv://shrutigarg749:<BFkmjX5yAojiBpc6>@mechanicform.zrwveqq.mongodb.net/test",{ useNewUrlParser: true })
-mongoose.connection.on("connected",() =>{
-    console.log("Database Connected");
-})
 
-mongoose.connection.on("Error",() =>{
-    console.log("Database Not Connected");
-})
+// app.use(cors());
+// app.use(express.json());
+// mongoose.connect("mongodb+srv://shrutigarg749:<BFkmjX5yAojiBpc6>@mechanicform.zrwveqq.mongodb.net/test",{ useNewUrlParser: true })
+// mongoose.connection.on("connected",() =>{
+//     console.log("Database Connected");
+// })
+
+// mongoose.connection.on("Error",() =>{
+//     console.log("Database Not Connected");
+// })
 
 // app.get("/",(req,res)=>{
 //     res.send("Hello World");
 // })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`);
+// })
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.use(bodyParser.json())
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// app.use(bodyParser.json())
+// app.use(cors());
+// app.use(express.json());
+// mongoose.connect("mongodb://0.0.0.0:27017/todo")
+// mongoose.connection.on("connected",() =>{
+//     console.log("Database Connected");
+// })
+
+// mongoose.connection.on("Error",() =>{
+//     console.log("Database Not Connected");
+// })
+
+
+
+
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const PORT = 4000;
+
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://0.0.0.0:27017/todo")
-mongoose.connection.on("connected",() =>{
-    console.log("Database Connected");
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-mongoose.connection.on("Error",() =>{
-    console.log("Database Not Connected");
-})
+mongoose.connect("mongodb+srv://shrutigarg749:<BFkmjX5yAojiBpc6>@mechanicform.zrwveqq.mongodb.net/test", { useNewUrlParser: true });
+mongoose.connection.on("connected", () => {
+  console.log("Database Connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Database Connection Error: " + err);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -112,16 +112,118 @@
 
 // export default Home;
 
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { useEffect, useRef } from "react";
-import image from "./About.jpg";
-import "./home.css";
-import Footer from "../Footer/Footer";
-import Accordion from "../accordion/Accordion";
+// import React from "react";
+// import { useLocation } from "react-router-dom";
+// import { useEffect, useRef } from "react";
+// import image from "./About.jpg";
+// import "./home.css";
+// import Footer from "../Footer/Footer";
+// import Accordion from "../accordion/Accordion";
+
+// const Home = () => {
+//   const location = useLocation();
+//   const carouselRef = useRef(null);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       carouselRef.current.querySelector(".carousel-control-next").click();
+//     }, 3000);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div>
+//       {/* Hello {location.state.id} */}
+
+//       <div
+//         id="carouselExample"
+//         className="carousel slide"
+//         ref={carouselRef}
+//         style={{
+//           marginTop: "2.5%",
+//           marginLeft: "8rem",
+//           marginRight: "3rem",
+//           height: "500px",
+//         }}
+//       >
+//         <div className="carousel-inner">
+//           <div className="carousel-item">
+//             <img
+//               src="https://images.pexels.com/photos/8804891/pexels-photo-8804891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=850&dpr=1"
+//               className="d-block carousel-image img-fluid"
+//               alt="NO image"
+//             />
+//           </div>
+//           <div className="carousel-item active">
+//             <img
+//               src="https://images.pexels.com/photos/1388278/pexels-photo-1388278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//               className="d-block carousel-image img-fluid"
+//               alt="..."
+//             />
+//           </div>
+//           <div className="carousel-item active">
+//             <img
+//               src="https://images.pexels.com/photos/5835349/pexels-photo-5835349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//               className="d-block carousel-image img-fluid"
+//               alt="..."
+//             />
+//           </div>
+//           <div className="carousel-item active">
+//             <img
+//               src="https://images.pexels.com/photos/4488667/pexels-photo-4488667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//               className="d-block carousel-image img-fluid"
+//               alt="..."
+//             />
+//           </div>
+//           <div className="carousel-item">
+//             <img
+//               src="https://images.pexels.com/photos/6140995/pexels-photo-6140995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//               className="d-block carousel-image img-fluid"
+//               alt="..."
+//             />
+//           </div>
+//         </div>
+//         <button
+//           className="carousel-control-prev"
+//           type="button"
+//           data-bs-target="#carouselExample"
+//           data-bs-slide="prev"
+//         >
+//           <span
+//             className="carousel-control-prev-icon"
+//             aria-hidden="true"
+//           ></span>
+//           <span className="visually-hidden">Previous</span>
+//         </button>
+//         <button
+//           className="carousel-control-next"
+//           type="button"
+//           data-bs-target="#carouselExample"
+//           data-bs-slide="next"
+//         >
+//           <span
+//             className="carousel-control-next-icon"
+//             aria-hidden="true"
+//           ></span>
+//           <span className="visually-hidden">Next</span>
+//         </button>
+//       </div>
+//       <Footer />
+//       {/* <Accordion/> */}
+
+//     </div>
+//   );
+// };
+
+// export default Home;
+import React,{ useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+import "./homepage.css";
 
 const Home = () => {
-  const location = useLocation();
+
+    const location = useLocation();
   const carouselRef = useRef(null);
 
   useEffect(() => {
@@ -132,10 +234,26 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {/* Hello {location.state.id} */}
-
-      <div
+    <div className="home-page">
+      {/* <header className="header">
+        <nav className="navbar">
+          <div className="navbar-brand">Mechanic Website</div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav> 
+      </header> */}
+        
+        
+        {/* <div
         id="carouselExample"
         className="carousel slide"
         ref={carouselRef}
@@ -150,6 +268,7 @@ const Home = () => {
           <div className="carousel-item">
             <img
               src="https://images.pexels.com/photos/8804891/pexels-photo-8804891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=850&dpr=1"
+              // src="slider2.jpg"
               className="d-block carousel-image img-fluid"
               alt="NO image"
             />
@@ -189,10 +308,7 @@ const Home = () => {
           data-bs-target="#carouselExample"
           data-bs-slide="prev"
         >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
         <button
@@ -201,18 +317,82 @@ const Home = () => {
           data-bs-target="#carouselExample"
           data-bs-slide="next"
         >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
-      </div>
-      <Footer />
-      {/* <Accordion/> */}
+      </div> */}
 
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="1000">
+      <img src="https://images.pexels.com/photos/1388278/pexels-photo-1388278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              className="d-block carousel-image img-fluid" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://images.pexels.com/photos/4488667/pexels-photo-4488667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              className="d-block carousel-image img-fluid" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.pexels.com/photos/8804891/pexels-photo-8804891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=850&dpr=1"
+               class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+      <section className="hero-section">
+        <div className="hero-content">
+          <h1>Welcome to Our Mechanic Website</h1>
+          <p>Your Trusted Destination for Car Repair and Maintenance</p>
+          <Link to="/services" className="btn btn-primary">Explore Services</Link>
+        </div>
+      </section>
+
+      <section className="services-section">
+        <h2>Our Services</h2>
+        <div className="service-card">
+          <div className="service-icon">
+            <i className="fa fa-wrench"></i>
+          </div>
+          <h3>Car Repair</h3>
+          <p>Expert car repair services to fix any mechanical or electrical issues.</p>
+        </div>
+        <div className="service-card">
+          <div className="service-icon">
+            <i className="fa fa-cogs"></i>
+          </div>
+          <h3>Vehicle Maintenance</h3>
+          <p>Comprehensive maintenance services to keep your vehicle in top condition.</p>
+        </div>
+        <div className="service-card">
+          <div className="service-icon">
+            <i className="fa fa-tire"></i>
+          </div>
+          <h3>Tire Services</h3>
+          <p>Professional tire repair, rotation, and replacement services.</p>
+        </div>
+      </section>
+
+      <section className="contact-section">
+        <h2>Contact Us</h2>
+        <p>Have a question or need assistance? Get in touch with our team.</p>
+        <Link to="/contact" className="btn btn-primary">Contact Now</Link>
+      </section>
+
+      <footer className="footer">
+        <div className="footer-content">
+          &copy; {new Date().getFullYear()} Mechanic Website. All rights reserved.
+        </div>
+      </footer>
     </div>
   );
 };
 
-export default Home;
+export default Home;
